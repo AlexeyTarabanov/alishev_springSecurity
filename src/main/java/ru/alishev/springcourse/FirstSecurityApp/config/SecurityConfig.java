c9cf074f0b061fe414676afdd5acc9228f20bfa2
Alexey Tarabanov <tarabanov@bk.ru>
2022-11-11 21:44:46 +0300

    Аутентификация || (UserDetailService)

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package ru.alishev.springcourse.FirstSecurityApp.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.alishev.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.alishev.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 /**
  * главный класс, в котором будем настраивать SS
@@ -13,18 +16,27 @@ import ru.alishev.springcourse.FirstSecurityApp.security.AuthProviderImpl;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     /**
      * настраивает аутентификацию
      * даем понять SS, что мы используем этот authProvider для аутентификации пользователей
      */
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    /**
+     * указываем, с помощью какого алгоритма мы шифруем свой пароль
+     * */
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        // пароль никак не шифруется
+        return NoOpPasswordEncoder.getInstance();
     }
 }
